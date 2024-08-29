e580cb08f7c032d375e79b26f5cb95d6f7bd9d7d
Kevin Stradtman <stradtkt22@gmail.com>
2024-08-28 22:34:25 -0400

    Initial Commit

---
+++ Game.cs
@@ -0,0 +1,35 @@
+namespace TreehouseDefense
+{
+    class Game
+    {
+        public static void Main()
+        {
+            Map map = new Map(8, 5);
+            try
+            {
+                MapLocation[] path = {
+                    new MapLocation(0, 2, map),
+                    new MapLocation(1, 2, map),
+                    new MapLocation(2, 2, map),
+                    new MapLocation(3, 2, map),
+                    new MapLocation(4, 2, map),
+                    new MapLocation(5, 2, map),
+                    new MapLocation(6, 2, map),
+                    new MapLocation(7, 2, map),
+                };
+            }
+            catch (OutOfBoundsException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (TreehouseDefenseException e)
+            {
+                Console.WriteLine("Unhandled TreehouseDefenseException");
+            }
+            catch (Exception e) 
+            {
+                Console.WriteLine("Unhandled Exception");
+            }
+        }
+    }
+}
---
+++ Path.cs
@@ -0,0 +1,11 @@
+namespace TreehouseDefense
+{
+    class Path
+    {
+        private readonly MapLocation[] _path;
+        public Path(MapLocation[] path) 
+        { 
+            _path = path;
+        }
+    }
+}
